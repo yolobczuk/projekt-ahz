da411113e84c1a2a37b0aea0cb7a5f4869186c1c
MG <windsower6@gmail.com>
2023-01-19 23:55:19 +0100

    zmiany w estymatorach KM i NA oraz wykresach

--- Notatki.R
+++ Notatki.R
@@ -3,42 +3,122 @@
 library(survival)
 library(gtools)
 library(survMisc)
-data <- survival::diabetic
+data <- survival::data
 
-# Kategoryzacja AGE
+
+###########################################################################################
+### Zmienna AGE
+#kategoryzacja zmiennej
 data$Age_cat=quantcut(data$age,q=4)
 summary(data)
 
-km = survfit(Surv(time, status) ~ trt, conf.type="plain", data=diabetic) # estymator KM
+#Estymator Kaplana-Meiera
+km = survfit(Surv(time, status) ~ Age_cat, conf.type="plain", data=data) # estymator KM
+summary(km)
+plot(km, col=c("blue","pink","red","green"))
+legend("topright",c("[1,10]","(10,16]","(16,30]","(30,58]"),lty=1,col=c("blue","pink","red","green"),bty="n")
+#estymator Nelsona-Aalena
+Nels=survfit(Surv(time, status) ~ Age_cat, conf.type ="log-log", type="fleming-harrington",data=data) # estymator NA
+summary(Nels)
+lines(Nels,plot(km, col=c("blue","pink","red","green")))
+Nels2=survfit(Surv(time,cens) ~ 1, conf.type ="plain", type="fleming-harrington",data=data) # estymator NA
+lines(Nels2,col="grey")
+
+#testy istotności
+test_petopeto=survdiff(Surv(time, status) ~ Age_cat, data=data, rho=1) # test Peto i Peto
+test_petopeto # wniosek: Przedział wiekowy nie jest istotnym predyktorem możliwości wystąpienia ślepoty (na poziomie istotnosci alfa=0,05)
+test_logrank=survdiff(Surv(time, status) ~ Age_cat, rho=0, data=data) # test log-rank
+test_logrank
+
+
+###########################################################################################
+### ZMIENNA TREATMENT - czy w ogóle było leczenie czy 0 -no treatment 1 - leczenie laserem
+#Estymator Kaplana-Meiera
+km = survfit(Surv(time, status) ~ trt, conf.type="plain", data=data) # estymator KM
 summary(km)
 plot(km)
 
-test.1=survdiff(Surv(time, status) ~ trt, data=diabetic, rho=1) # test Peto i Peto
-test.1 # wniosek: zmienna inst nie jest istotnym predyktorem (na poziomie istotnosci alfa=0,05)
-test.1=survdiff(Surv(time, status) ~ trt, rho=0, data=diabetic) # test log-rank
-test.1
+test_petopeto=survdiff(Surv(time, status) ~ trt, data=data, rho=1) # test Peto i Peto
+test_petopeto # wniosek: zmienna trt nie jest istotnym predyktorem (na poziomie istotnosci alfa=0,05)
+test_logrank=survdiff(Surv(time, status) ~ trt, rho=0, data=data) # test log-rank
+test_logrank
 
-km = survfit(Surv(time, status) ~ laser, conf.type="plain", data=diabetic) # estymator KM
+#Obydwa testy (Peto Peto oraz log rank) przy poziomie istotności = 0.05 wskazują, że leczenie oka laserem wpływa istotnie
+# na nie wystąpienie ślepoty w leczonym oku. 
+#estymator Nelsona-Aalena
+Nels=survfit(Surv(time, status) ~ trt, conf.type ="log-log", type="fleming-harrington",data=data) # estymator NA
+summary(Nels)
+lines(Nels,col="blue")
+Nels2=survfit(Surv(time,cens) ~ 1, conf.type ="plain", type="fleming-harrington",data=data) # estymator NA
+lines(Nels2,col="grey")
+
+
+############################################################################################
+### ZMIENNA LASER - typ laseru xenon albo argon
+
+km = survfit(Surv(time, status) ~ laser, conf.type="plain", data=data) # estymator KM
 summary(km)
 plot(km)
 
 #estymator Nelsona-Aalena
-Nels=survfit(Surv(time, status) ~ trt, conf.type ="log-log", type="fleming-harrington",data=diabetic) # estymator NA
+Nels=survfit(Surv(time, status) ~ laser, conf.type ="log-log", type="fleming-harrington",data=data) # estymator NA
 summary(Nels)
 lines(Nels,col="blue")
-Nels2=survfit(Surv(time,cens) ~ 1, conf.type ="plain", type="fleming-harrington",data=diabetic) # estymator NA
+Nels2=survfit(Surv(time,cens) ~ 1, conf.type ="plain", type="fleming-harrington",data=data) # estymator NA
 lines(Nels2,col="grey")
 
-#Test dla zmiennej laser. Widac, ze wartosc p jest wysoka. Zmienne nie sa rozne i nie wplywa na dlugosc zycia.
-test.1=survdiff(Surv(time, status) ~ laser, data=diabetic, rho=1) # test Peto i Peto
-test.1 # wniosek: zmienna inst nie jest istotnym predyktorem (na poziomie istotnoÅci alfa=0,05)
-test.1=survdiff(Surv(time, status) ~ laser, rho=0, data=diabetic) # test log-rank
-test.1
+#Test dla zmiennej laser. Widac, ze wartosc p jest wysoka. oznacza to, że typ użytego lasera nie wpływa istotnie na różnice w wystąpieniu ślepoty.
+test_petopeto=survdiff(Surv(time, status) ~ laser, data=data, rho=1) # test Peto i Peto
+test_petopeto # wniosek: zmienna inst nie jest istotnym predyktorem (na poziomie istotnoÅci alfa=0,05)
+test_logrank=survdiff(Surv(time, status) ~ laser, rho=0, data=data) # test log-rank
+test_logrank
 
-# To powyzej powtarzyc dla wszystkich predyktorow
+
+#############################################################################################
+### ZMIENNA EYE - które oko było leczone
+
+km = survfit(Surv(time, status) ~ eye, conf.type="plain", data=data) # estymator KM
+summary(km)
+plot(km, col=c("blue","pink","red","green","yellow","black"))
+legend("topright",c("6","8","9","10","11","12"),lty=1,col=c("blue","pink","red","green","yellow","black"),bty="n")
+
+#estymator Nelsona-Aalena
+Nels=survfit(Surv(time, status) ~ eye, conf.type ="log-log", type="fleming-harrington",data=data) # estymator NA
+lines(Nels2,col="grey")
+lines(Nels,col=c("blue","pink","red","green","yellow","black"))     ### NIE WIEM JAK TO WYKONAĆ PÓKI CO
+Nels2=survfit(Surv(time,cens) ~ 1, conf.type ="plain", type="fleming-harrington",data=data) # estymator NA
+lines(Nels2,col="grey")
+
+#W przypadku obydwóch testów, przy przyjętym poziomie istotności - 0.05 nie wystąpiła statystycznie istotna różnica
+# pomiędzy leczeniem prawo a lewego oka 
+test_petopeto=survdiff(Surv(time, status) ~ eye, data=data, rho=1) # test Peto i Peto
+test_petopeto # wniosek: zmienna inst nie jest istotnym predyktorem (na poziomie istotnoÅci alfa=0,05)
+test_logrank=survdiff(Surv(time, status) ~ eye, rho=0, data=data) # test log-rank
+test_logrank
+
+#############################################################################################
+### ZMIENNA RISK - grupa ryzyka
+
+km = survfit(Surv(time, status) ~ risk, conf.type="plain", data=data) # estymator KM
+summary(km)
+plot(km)
+
+#estymator Nelsona-Aalena
+Nels=survfit(Surv(time, status) ~ risk, conf.type ="log-log", type="fleming-harrington",data=data) # estymator NA
+summary(Nels)
+lines(Nels)
+Nels2=survfit(Surv(time,cens) ~ 1, conf.type ="plain", type="fleming-harrington",data=data) # estymator NA
+lines(Nels2)
+
+# Przy przyjętym poziomie istotności = 0.05 grupa ryzyka miała istotny wpływ na wystąpienie ślepoty w oku
+test_petopeto=survdiff(Surv(time, status) ~ risk, data=data, rho=1) # test Peto i Peto
+test_petopeto # wniosek: zmienna inst nie jest istotnym predyktorem (na poziomie istotnoÅci alfa=0,05)
+test_logrank=survdiff(Surv(time, status) ~ risk, rho=0, data=data) # test log-rank
+test_logrank
+
 
 #library(survMisc) # zbior testÃow dla roznych wag
-t1=survfit(Surv(time, status) ~ trt,data=diabetic)
+t1=survfit(Surv(time, status) ~ trt,data=data)
 comp(ten(t1),p=c(0,1,1,0.5,0.5),q=c(1,0,1,0.5,2)) # roz¼ne wagi w testach Fleminga-Harringtona
 # jako druga lista w przypadku zmiennej dychotomicznej sÄ wyÅwietlane testy Renyi
 #kategoryzacja zmiennej wedÅug kwantyli i porÃ³wnywanie krzywych przeÅ¼ycia dla grup kwartylowych
@@ -60,13 +140,13 @@ cbind(b$W, b$pChisq, c$pSupBr)
 plot(survfit(Surv(time,status)~risk, data=data[data$risk==9 | data$risk==8,]))
 
 
-Cox=coxph(Surv(time, status) ~ trt+laser+age+eye+risk, data=diabetic)
+Cox=coxph(Surv(time, status) ~ trt+laser+age+eye+risk, data=data)
 summary(Cox)
 library(MASS)
 stepAIC(Cox,direction="backward")
 #Najlepiej wychodzi zestawinie zmiennych trt, eyeright, risk. Wartosc p jest najnizsza.
 
-Cox=coxph(Surv(time,status)~trt+eye+risk, data=diabetic)
+Cox=coxph(Surv(time,status)~trt+eye+risk, data=data)
 summary(Cox)
 
 library(survMisc)
